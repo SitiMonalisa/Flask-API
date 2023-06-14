f0c407b4a2e2612167c661bddc9343b6cfcb2ae6
SitiMonalisa <93410991+SitiMonalisa@users.noreply.github.com>
2023-06-14 16:56:00 +0700

    Update main.py

--- main.py
+++ main.py
@@ -50,5 +50,5 @@ def predict():
 
     if __name__ == '__main__':
     # Menentukan port berdasarkan variabel lingkungan PORT atau menggunakan port default 8080
-    port = int(os.environ.get('PORT', 8000))
+    port = int(os.environ.get('PORT', 8080))
     app.run(host='0.0.0.0', port=port)
